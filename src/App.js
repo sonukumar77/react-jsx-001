01c13f1f4a9d023cfa87dc667d596ff6055bcda7
Sonu Kumar <82059136+sonukumar77@users.noreply.github.com>
2022-11-03 11:52:18 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import AboutUs from "./components/AboutUs";
+function App() {
+  return (
+    <div className="container">
+      <AboutUs />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/AboutUs.js
@@ -0,0 +1,14 @@
+
+const AboutUs = () => {
+    return(
+        <div className="container">
+        <h1>About Us</h1>
+        <p>At Zappos.com, our purpose is simple: to live and deliver WOW.</p>
+        <p>Twenty years ago, we began as a small online retailer that only sold shoes. Today, we still sell shoes - as well as clothing, handbags, accessories, and more. That "more" is providing the very best customer service, customer experience, and company culture. We aim to inspire the world by showing it's possible to simultaneously deliver happiness to customers, employees, vendors, shareholders, and the community in a long-term, sustainable way.</p>
+        <p>We hope that in the future people won't even realize we started selling shoes online. Instead, they'll know Zappos as a service company that just happens to sell ______.</p>
+        </div>
+    )
+}
+
+export default AboutUs;
+
